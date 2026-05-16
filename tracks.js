d0958758044fc4084c5dceb731668ab3aa2610b9
mikedezilva <mikedezilva@gmail.com>
2026-05-16 22:06:51 +0530

    soma update

--- tracks.js
+++ tracks.js
@@ -1,37 +1,37 @@
 const tracks = [
     {
         title: "Track 1",
-        artist: "Ryan Somarathne X Mike De Zilva",
+        artist: "Ryan Soma X Mike De Zilva",
         url: "assets/audio/Track 1.wav",
         cover: "assets/cover/Silence-Album-art.jpeg"
     },
     {
         title: "Track 2",
-        artist: "Ryan Somarathne X Mike De Zilva",
+        artist: "Ryan Soma X Mike De Zilva",
         url: "assets/audio/Track 2.wav",
         cover: "assets/cover/Silence-Album-art.jpeg"
     },
     {
         title: "Track 3",
-        artist: "Ryan Somarathne X Mike De Zilva",
+        artist: "Ryan Soma X Mike De Zilva",
         url: "assets/audio/Track 3.wav",
         cover: "assets/cover/Silence-Album-art.jpeg"
     },
     {
         title: "Track 4",
-        artist: "Ryan Somarathne X Mike De Zilva",
+        artist: "Ryan Soma X Mike De Zilva",
         url: "assets/audio/Track 4.wav",
         cover: "assets/cover/Silence-Album-art.jpeg"
     },
     {
         title: "Track 5",
-        artist: "Ryan Somarathne X Mike De Zilva",
+        artist: "Ryan Soma X Mike De Zilva",
         url: "assets/audio/Track 5.wav",
         cover: "assets/cover/Silence-Album-art.jpeg"
     },
     {
         title: "Track 6",
-        artist: "Ryan Somarathne X Mike De Zilva",
+        artist: "Ryan Soma X Mike De Zilva",
         url: "assets/audio/Track 6.wav",
         cover: "assets/cover/Silence-Album-art.jpeg"
     }
